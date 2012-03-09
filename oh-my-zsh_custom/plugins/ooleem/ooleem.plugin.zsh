011c8e3d389bc9d1310a01a8cedc44791cdbfa71
Hoon <ooleem@gmail.com>
2012-03-08 22:36:03 -0800

    Remove ggraph. Use git graph instead

--- oh-my-zsh_custom/plugins/ooleem/ooleem.plugin.zsh
+++ oh-my-zsh_custom/plugins/ooleem/ooleem.plugin.zsh
@@ -1,6 +1,4 @@
 # Aliases
-alias ggraph='git log --graph --date-order -C -M --pretty=format:"<%h> %ad [%an] %Cgreen%d%Creset %s" --all --date=short'
-
 c() { cd ~/Projects/$1;  }
 
 _c() { _files -W ~/Projects -/; }
